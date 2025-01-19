8f979f89d0e1785b3b4a8571b92080286c45b02b
rajaharris55 <rajaharris55@gmail.com>
2025-01-20 01:19:16 +0500

    axios auth register

--- src/views/authentication/authForms/AuthRegister.js
+++ src/views/authentication/authForms/AuthRegister.js
@@ -1,60 +1,113 @@
-import React from 'react';
-import { Box, Typography, Button, Divider } from '@mui/material';
-import { Link } from 'react-router';
+import React, { useState } from 'react';
+import { Box, Typography, Button, Stack, Divider } from '@mui/material';
+
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import CustomTextField from '../../../components/forms/theme-elements/CustomTextField';
 import CustomFormLabel from '../../../components/forms/theme-elements/CustomFormLabel';
-import { Stack } from '@mui/system';
 import AuthSocialButtons from './AuthSocialButtons';
 
-const AuthRegister = ({ title, subtitle, subtext }) => (
-  <>
-    {title ? (
-      <Typography fontWeight="700" variant="h3" mb={1}>
-        {title}
-      </Typography>
-    ) : null}
+const API = 'http://localhost:3000';
 
-    {subtext}
-    <AuthSocialButtons title="Sign up with" />
+const AuthRegister = ({ title, subtitle, subtext }) => {
+  const [register, setRegister] = useState({
+    username: '',
+    password: '',
+    role: 'agent',
+  });
 
-    <Box mt={3}>
-      <Divider>
-        <Typography
-          component="span"
-          color="textSecondary"
-          variant="h6"
-          fontWeight="400"
-          position="relative"
-          px={2}
-        >
-          or sign up with
+  const handleInput = (event) => {
+    setRegister({ ...register, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(`${API}/api/register`, register)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      {title ? (
+        <Typography fontWeight="700" variant="h3" mb={1}>
+          {title}
         </Typography>
-      </Divider>
-    </Box>
+      ) : null}
 
-    <Box>
-      <Stack mb={3}>
-        <CustomFormLabel htmlFor="name">Username</CustomFormLabel>
-        <CustomTextField id="name" variant="outlined" fullWidth />
-        <CustomFormLabel htmlFor="email">Email Adddress</CustomFormLabel>
-        <CustomTextField id="email" variant="outlined" fullWidth />
-        <CustomFormLabel htmlFor="password">Password</CustomFormLabel>
-        <CustomTextField id="password" variant="outlined" fullWidth />
+      {subtext}
+
+      <AuthSocialButtons title="Sign up with" />
+      <Box mt={3}>
+        <Divider>
+          <Typography component="span" color="textSecondary" variant="h6" fontWeight="400" px={2}>
+            or sign up with
+          </Typography>
+        </Divider>
+      </Box>
+
+      <Stack component="form" onSubmit={handleSubmit}>
+        <Box>
+          <CustomFormLabel htmlFor="name">Name</CustomFormLabel>
+          <CustomTextField
+            id="username"
+            name="username"
+            variant="outlined"
+            fullWidth
+            value={register.username}
+            onChange={handleInput}
+          />
+        </Box>
+        <Box>
+          <CustomFormLabel htmlFor="email">Email</CustomFormLabel>
+          <CustomTextField
+            id="email"
+            name="email"
+            variant="outlined"
+            fullWidth
+            onChange={handleInput}
+          />
+        </Box>
+        <Box>
+          <CustomFormLabel htmlFor="password">Password</CustomFormLabel>
+          <CustomTextField
+            id="password"
+            name="password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            value={register.password}
+            onChange={handleInput}
+          />
+        </Box>
+        <Stack justifyContent="space-between" direction="row" alignItems="center" my={2}>
+          <Typography
+            component={Link}
+            to="/auth/login"
+            fontWeight="500"
+            sx={{
+              textDecoration: 'none',
+              color: 'primary.main',
+            }}
+          >
+            Already have an account?
+          </Typography>
+        </Stack>
+        <Box>
+          <Button color="primary" variant="contained" size="large" fullWidth type="submit">
+            Sign Up
+          </Button>
+        </Box>
       </Stack>
-      <Button
-        color="primary"
-        variant="contained"
-        size="large"
-        fullWidth
-        component={Link}
-        to="/auth/login"
-      >
-        Sign Up
-      </Button>
-    </Box>
-    {subtitle}
-  </>
-);
+      {subtitle}
+    </>
+  );
+};
 
 export default AuthRegister;
